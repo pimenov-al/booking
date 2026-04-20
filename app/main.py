963897cba75ccd5c9dd0cc6e0acb6b082b60c114
pimenov-al <pimenov.al@gmail.com>
2026-04-20 18:48:14 +0300

    add app log() for test
    1. Добавил из раннего урока модуль тестирования функции log.
    2. Там используются 2 пакета - обновил requirements.txt

---
+++ app/main.py
@@ -0,0 +1,51 @@
+from typing import Union
+import math
+from app.error import InvalidInputException
+
+numeric = Union[int, float]
+
+
+class Calculator:
+
+    @staticmethod
+    def sum(*args):
+        for arg in args:
+            if not isinstance(arg, numeric):
+                raise TypeError
+        return sum(args)
+
+    @staticmethod
+    def subtract(a: numeric, b: numeric) -> numeric:
+        return a - b
+
+    @staticmethod
+    def multiply(a: numeric, b: numeric) -> numeric:
+        return a * b
+
+    @staticmethod
+    def divide(a: numeric, b: numeric) -> numeric:
+        return a / b
+
+    def log(self, a: numeric, base: numeric) -> numeric:
+        # TODO: cover this method with unit-tests
+        if not (isinstance(a, numeric) and isinstance(base, numeric)):
+            raise TypeError
+
+        #if a > 0 and a != 1 and base > 0:           # a != 1 - лишнее
+        if a > 0 and base > 0:
+            return math.log(a, base)
+        else:
+            raise InvalidInputException(self.log, a, base)
+
+
+calc = Calculator()
+
+# проверка что все класс работает - OK
+# print('проверка 6 + 2 = ', calc.sum(6, 2))       #8
+# print('проверка 6 : 2 = ', calc.divide(6, 2))    #3.0
+# print('проверка 6 * 2 = ', calc.multiply(6, 2))  #12
+# print('проверка log2-8 = ', calc.log(8, 2))    #3
+
+#d = calc.log('a', 0)
+#print(d) # здесь будет ошибка
+
---
+++ tests/test_calc.py
@@ -0,0 +1,110 @@
+import math
+import unittest
+from parameterized import parameterized
+from app.main import Calculator
+from math import inf
+# список импортов в видео воркшопа см на время 33:33
+
+
+class TestCalculator(unittest.TestCase):
+    # здесь запускаем один экз app на все тесты
+    def setUp(self) -> None:
+        self.calc = Calculator()
+
+    # здесь все зачищаем после отработки тестов
+    def tearDown(self) -> None:
+        ...
+
+    @parameterized.expand(
+        # 1. arrange
+        # задаем варианты параметров (a,b, ожид.рез)
+        # для каждого теста, который идет дальше
+        # как это (@parameterized.expand) попадает в тест (def test_sum) ????
+        [
+            ("integers", 2, 3, 5),
+            ("floats", 2.5, 3.1, 5.6),
+            ("negative", -2.5, 3.0, 0.5)
+        ]
+    )
+    def test_sum(self, name, a, b, expected_result):
+        # 2. act
+        # получаем факт. результат выполнения функции
+        actual_result = self.calc.sum(a, b)
+
+        # 3. assert
+        # сравниваем факт. и ожидаемый результаты
+        self.assertEqual(actual_result, expected_result)
+
+    @parameterized.expand([
+        ("strings", 'aaa', 'bbb', TypeError),
+        ("int_None", 1, None, TypeError),
+        ("None_float", None, 1.1, TypeError),
+        ("None_None", None, None, TypeError)
+
+    ])
+    def test_sum_invalid_values(self, name, a, b, expected_result):
+        with self.assertRaises(expected_result):
+            self.calc.sum(a, b)
+
+    @parameterized.expand([
+        ("list_integers", [1, 2, 3, 4], 10),
+        ("list_empty", [], 0),
+        ("list_single", [1], 1)
+    ])
+    def test_sum_list(self, name, a, expected_result):
+        # 2. act
+        actual_result = self.calc.sum(*a)
+        # 3. assert
+        self.assertEqual(actual_result, expected_result)
+
+    @parameterized.expand([
+        ("tuple_integers", (1, 2, 3, 4), 10),
+        ("tuple_empty", (), 0),
+        ("tuple_single", (1,), 1),
+        ("set_integers", {1, 2, 3, 4}, 10),
+        ("set_empty", {}, 0),
+        ("set_single", {1}, 1),
+    ])
+    def test_sum_tuple(self, name, a, expected_result):
+        # 2. act
+        actual_result = self.calc.sum(*a)
+        # 3. assert
+        self.assertEqual(actual_result, expected_result)
+
+    def test_multiply(self):
+        a = 5
+       # b = 0 # OK
+        b = 0.000000005 #failed
+
+        actual_result = self.calc.multiply(a, b)
+        expected_result = 0 # OK, = 1 # failed
+        # self.assertEqual(actual_result, expected_result) # для b = 0.000000005
+                                # тест будет failed, т.к. резалт будет не ровный ноль, нужен assertAlmostEqual
+
+        self.assertAlmostEqual(actual_result, expected_result) # OK
+        #здесь по дефолту сравниваются числа до 7 знака, places может изменить это
+        #self.assertAlmostEqual(actual_result, expected_result, places=8) # так упадет
+
+    def test_divide(self):
+        a = 5
+        b = 0
+
+        expected_result = ZeroDivisionError
+
+        with self.assertRaises(expected_result):
+            self.calc.divide(a, b)
+
+    def test_divide_inf(self):
+        a = inf
+        b = inf
+
+        expected_result = None
+        actual_result = self.calc.divide(a, b)
+
+        self.assertNotEqual(actual_result, expected_result)
+        self.assertIsInstance(actual_result, type(math.inf))
+        self.assertIsInstance(actual_result, float)
+
+# это наш ранер
+if __name__ == "__main__":
+    unittest.main()

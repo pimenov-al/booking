6cfa32643944f9cf205ac3e2d35365433a0c7b8b
pimenov-al <pimenov.al@gmail.com>
2026-04-20 17:25:39 +0300

    c0415 fix
    fix C0415 в manage.py

--- manage.py
+++ manage.py
@@ -3,18 +3,22 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+# скопировал импорт из блока try.
+# для устранения ошибки C0415 import outside toplevel
+# чтобы пройти pylint в ci-cd в github
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+#    try:
+#        from django.core.management import execute_from_command_line
+#    except ImportError as exc:
+#        raise ImportError(
+#            "Couldn't import Django. Are you sure it's installed and "
+#            "available on your PYTHONPATH environment variable? Did you "
+#            "forget to activate a virtual environment?"
+#        ) from exc
     execute_from_command_line(sys.argv)
 
 

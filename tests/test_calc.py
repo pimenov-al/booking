a75d1667a415ef18f072b6a1fe396580f6617efe
pimenov-al <pimenov.al@gmail.com>
2026-04-20 19:20:51 +0300

    fix unused argument  'name'
    fix W0613: Unused argument 'name'

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -39,7 +39,8 @@ class TestCalculator(unittest.TestCase):
             ("negative", -2.5, 3.0, 0.5)
         ]
     )
-    def test_sum(self, name, a, b, expected_result):
+    #def test_sum(self, name, a, b, expected_result):
+    def test_sum(self, a, b, expected_result):
         """
         Описание функции test_sum
         """
@@ -70,7 +71,8 @@ class TestCalculator(unittest.TestCase):
         ("list_empty", [], 0),
         ("list_single", [1], 1)
     ])
-    def test_sum_list(self, name, a, expected_result):
+    #def test_sum_list(self, name, a, expected_result):
+    def test_sum_list(self, a, expected_result):
         """
         Описание функции test_sum_list
         """
@@ -87,7 +89,8 @@ class TestCalculator(unittest.TestCase):
         ("set_empty", {}, 0),
         ("set_single", {1}, 1),
     ])
-    def test_sum_tuple(self, name, a, expected_result):
+    #def test_sum_tuple(self, name, a, expected_result):
+    def test_sum_tuple(self, a, expected_result):
         """
         Описание функции test_sum_tuple
         """
